58a90cc170aabb741fee1313ad50b3fdb909e07f
nicobojacarojas <nicobojacarojas@hotmail.com>
2018-04-19 07:12:16 -0500

    primera version

---
+++ diferenciacion.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using std::cout;
+using std::endl;
+double iniciar(double *u);
+int main
+{
+	double dt = 0.01;
+	double dx = 0.1;
+	double x_max = 2.0;
+	double T = 0.5;
+	double *u_i[x_max/dx];
+	double *u_i1[T/dt]; 
+	for (int j = 1; j < T/dt; j++)
+	{
+		if (x<0.25 || x>0.75)
+		{
+
+		}
+	}
+}
+
+double iniciar(double*u)
+{
+
+}
